e79102406c5738f6b65a52723bf76e1df67efd0a
EmilianPostolache <emilianpos@gmail.com>
2022-03-07 16:56:17 +0100

    At part 1.d

--- 03_ex1/utils.py
+++ 03_ex1/utils.py
@@ -1,4 +1,4 @@
-from mpl_toolkits.mplot3d import Axes3D
+# from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.patches import FancyArrowPatch
 from mpl_toolkits.mplot3d import proj3d
 import numpy as np
